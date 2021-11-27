5a400c726cb5254e955b2bffed15d555edbb9323
shalini-jeyra <shalinijeyra@gmail.com>
2021-11-27 13:51:57 +0530

    added app styles

---
+++ lib/styles/colors.dart
@@ -0,0 +1,39 @@
+part of styles;
+
+class AppColors {
+  static const Color primary = Color.fromARGB(
+    255,
+    255,
+    255,
+    255,
+  );
+  static const Color secondary = Color.fromARGB(
+    255,
+    0,
+    0,
+    0,
+  );
+  static const Color ternary = Color.fromARGB(
+    255,
+    15,
+    158,
+    13,
+  );
+}
+
+class TextColors {
+
+  static const Color primary = Color.fromARGB(
+    255,
+    0,
+    0,
+    0,
+  );
+    
+   static const Color secondary = Color.fromARGB(
+    255,
+    255,
+    255,
+    255,
+  );
+}
